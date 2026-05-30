0cfb5babd665ea611038fa89e01fd65285c2586a
Sri Sai Amrutha Hasini Adunutala <amruthahasini2008@gmail.com>
2026-05-30 11:58:37 +0530

    Add files via upload

---
+++ L9A4.py
@@ -0,0 +1,22 @@
+class parrot :
+
+    def __init__(self, name , age):
+        self.name = name 
+        self.age = age
+
+
+    def sing (self , song) :
+        return '{} is singing {}'.format(self.name,song)
+    
+    def dance (self) :
+        return '{} is dancing'.format(self.name)
+    
+
+blu = parrot('blu',10)
+
+print(blu.sing('happy'))
+print(blu.dance())
+
+        
+
+        
